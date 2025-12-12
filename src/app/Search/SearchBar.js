ebcc4bc334a9dc37a4a7f5c1701a97f06776552c
DavidOConnor1 <146002230+DavidOConnor1@users.noreply.github.com>
2025-12-12 16:44:50 +0000

    finished the search bar

--- src/app/Search/SearchBar.js
+++ src/app/Search/SearchBar.js
@@ -19,7 +19,14 @@ export default function SearchBar({ onSearch, intialQuery = ''}){
                 type="text"
                 value={query}
                 onChange={(e) => setQuery(e.target.value)}
+                placeholder="Search any Pokemon Cards (Pikachu, Eevee)"
+                className="flex-grow px-4 py-3 border border-gray-300 rounded-lg focus:ring-2 focus:ring-blue-500 focus:border-transparent" />
+                <button
+                type="submit"
+                className="px-6 py-3 bg-blue-600 text-white rounded-lg hover:bg-blue-700 transition-colors">
+                    Search
+                </button>
             </div>
         </form>
-    )
+    );
 }//end search bar
