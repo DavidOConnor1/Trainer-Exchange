1c86eba10bed1f3c1c08484bb61c85a575538d25
DavidOConnor1 <146002230+DavidOConnor1@users.noreply.github.com>
2026-01-23 11:47:59 +0000

    I have created a fetch users and handle submit. I created the skeleton of the handle delete

--- src/app/User/users.js
+++ src/app/User/users.js
@@ -12,8 +12,36 @@ export default function UsersPage() { //start function
   }, []);
 
   const fetchUsers = async () => {//start fetchUsers
-    
-  }//end fetch users
+    try{//start try/catch/finally
+        const data = await api.getUsers(); //data will hold the fetched user data
+        setUsers(data); //will set the users with the retrieved data
+    } catch(error){
+        console.error('Failed to fetch users: ', error);
+
+    } finally {
+        setLoading(false);
+    }//end try/catch/finally
+  };//end fetch users
+
+  const handleSubmit = async (e) => { //start handle submit
+    e.preventDefault();
+    try{ //start try/catch
+        const createdUser = await api.createUser(newUser);
+        setUsers([...users, createdUser]);
+        setNewUser({name: '', email: ''});
+
+    } catch (error){
+        console.error('failed to created user: ', error);
+    }//end try/catch
+  };//end handle submit
+
+  const handleDelete = async (id) => { //start handle delete
+    try{// start try/catch
+        
+    }catch(error) {
+
+    }//end try/catch
+  }; //end handle delete
 
 
 }//end function
