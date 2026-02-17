2d0079493104beb9a1edbab9fb44348c72970718
DavidOConnor1 <146002230+DavidOConnor1@users.noreply.github.com>
2026-02-17 21:39:35 +0000

    Adding some css for when the user logs into their account. Allow them to see their own username (soon) and go into user settings

--- src/app/User/page.js
+++ src/app/User/page.js
@@ -8,19 +8,25 @@ export default function UsersPage() {
   if(loading) return <div> ...loading</div>
 
   return (
-    <div className="">
-      {user ? (
-        <div>
-          <h1>Welcome, {user.email}!</h1>
-          <button onClick={() => signOut()}>Sign Out</button>
+  <div className="">
+    {user ? (
+      <div className="fixed z-20 w-full h-16 max-w-lg -translate-x-1/2 bg-white border border-gray-200 rounded-full top-4 left-1/2 dark:bg-gray-700 dark:border-gray-600">
+          <div className="grid h-full max-w-lg grid-cols-[70%_30%] mx-auto px-4">
+            <div className="flex items-center">
+              <h1 className="text-sm sm:text-base truncate">
+                Welcome, {user.email}!
+              </h1>
+            </div>
+            <div className="flex items-center justify-center">
+             <h1>Hi</h1>
+            </div>
+          </div>
         </div>
-      ) : (
-        <>
-          
-          <Auth />
-        </>
-      )}
-    </div>
-  );
-
+    ) : (
+      <>
+        <Auth />
+      </>
+    )}
+  </div>
+);
 }//end function
