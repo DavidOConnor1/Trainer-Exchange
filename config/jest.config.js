c7024db53029bdac57bda81b8f042bdfafada596
DavidOConnor1 <146002230+DavidOConnor1@users.noreply.github.com>
2026-04-16 16:57:15 +0100

    had to update the configuration and change the import of the dependency to work with the project

--- config/jest.config.js
+++ config/jest.config.js
@@ -2,11 +2,9 @@
 const nextJest = require('next/jest')
 
 const createJestConfig = nextJest({
-  // Provide the path to your Next.js app to load next.config.js and .env files
   dir: './',
 })
 
-// Add any custom config to be passed to Jest
 const customJestConfig = {
   testEnvironment: 'jsdom',
   setupFilesAfterEnv: ['<rootDir>/test/setup/jest.setup.js'],
@@ -21,9 +19,13 @@ const customJestConfig = {
     'hooks/**/*.js',
     'lib-supa/**/*.js',
     'components/**/*.js',
+    'lib/**/*.js',
     '!**/node_modules/**'
+  ],
+  // Add this to transform ES modules
+  transformIgnorePatterns: [
+    '/node_modules/(?!(sanitize-html)/)'
   ]
 }
 
-// createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
 module.exports = createJestConfig(customJestConfig)
